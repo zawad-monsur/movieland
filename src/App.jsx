a016aacb9a695d3f702b11122d275ab85d8b7195
K. M. Zawad Monsur <zawad.monsur1@gmail.com>
2022-11-01 00:47:17 +0600

    Making single movie card with static data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
-import MovieCard from './MovieCard'
 import './App.css'
 
 const API_URL = 'http://www.omdbapi.com?apikey=5f2114e'
+
+const movie1 = {
+    Title: 'Enter the Void',
+    Year: '2009',
+    imdbID: 'tt1191111',
+    Type: 'movie',
+    Poster: 'https://m.media-amazon.com/images/M/MV5BMGQwMWRmODEtYzYwMy00MzFiLWJhYWUtNWNkNWM0NWZhODBiXkEyXkFqcGdeQXVyNjQyMjcwNDM@._V1_SX300.jpg'
+}
 
 const App = () => {
     useEffect(() => {
@@ -13,12 +20,34 @@ const App = () => {
     const searchMovies = async title => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json()
-        console.log(data)
+        console.log(data.Search)
     }
 
     return (
-        <div>
-            <h1>App</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+
+            <div className='search'>
+                <input placeholder='Search for movies' value='' onChange={() => {}} />
+                {/* <img src={SearchIcon} alt='search' onClick={() => {}} /> */}
+            </div>
+
+            <div className='container'>
+                <div className='movie'>
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
